82b43642e2c012d7fdb2180d587383f99cd8caa5
ethan fregien <ethanfregien@gmail.com>
2021-09-14 15:16:33 -0500

    added password character prompt

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
+// variable arrays for generation
 var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var letUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var letLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var specialChar = ["%", "!", "&", ",","-", "*", "+",  ".", "<", "/", ">", "?","~"];
 
+var confirmUpper;
+var confirmLower;
+var confirmSpecial;
+var confirmNumber;
+var confirmLength = "";
 
-// Assignment code here
+function generatePassword() {
+  var confirmLength=(prompt("How many characters should your password contain? Please pick between 8 and 128 characters."));
 
-
+  while(confirmLength >= 129 || confirmLength <=7) {
+    alert("Password must be between 8-128 characters! Please try again.");
+  }
+}
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
