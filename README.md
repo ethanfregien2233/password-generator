6cf69773a08027a6473891c1242efb6bc8634c8b
ethan fregien <ethanfregien@gmail.com>
2021-09-14 16:25:21 -0500

    started readme

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# password-generator
+For this assignment, I created a password generator using javascript. The generator can produce a password that is 8-128 characters long and gives users the choice on what type of characters they would like to include in the password.
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// variable arrays for generation
+
 var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var letUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var letLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
@@ -32,6 +32,32 @@ function generatePassword() {
     var confirmSpecial = confirm("Press OK if you would like to include special characters");
     var confirmNumber = confirm("Press OK if you would like to include numbers");
 } 
+
+    var passwordChar = []
+
+    if (confirmUpper) {
+      passwordChar = passwordChar.concat(letUpper)
+    }  
+
+    if (confirmLower) {
+      passwordCharacters = passwordChar.concat(letLower)
+    }
+
+    if (confirmSpecial) {
+      passwordChar = passwordChar.concat(specialChar)
+    }
+
+    if (confirmNumber) {
+      passwordChar = passwordChar.concat(number)
+    }
+    
+    var password = ""
+      
+      for (var i = 0; i < confirmLength; i++) {
+        password = password + passwordChar[Math.floor(Math.random() * passwordChar.length)];
+        console.log(password)
+      }
+      return password;
 }
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
